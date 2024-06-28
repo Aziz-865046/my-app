a0b66aa31257485d7c475af01c9ed7601ce2bf9c
Aziz-865046 <aziz865046@outlook.com>
2024-06-28 08:02:40 +0530

    darkmode error

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -41,13 +41,22 @@ export default function TextForm(props) {
   const [text, setText] = useState("");
   return (
     <>
-      <div className="container">
+      <div
+        className="container"
+        style={{
+          color: props.mode === "dark" ? "white" : "black",
+        }}
+      >
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea
             className="form-control"
             value={text}
             onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
             id="myBox"
             rows="8"
           ></textarea>
@@ -58,14 +67,14 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-1" onClick={handleLoClick}>
           Convert to lowercase
         </button>
+        <button className="btn btn-primary mx-1" onClick={handleCapitalize}>
+          Convert to Capitalize Each Word
+        </button>
         <button className="btn btn-primary mx-1" onClick={handleClClick}>
           Text to Clear
         </button>
         <button className="btn btn-primary mx-1" onClick={handleinverseclick}>
           Reverce
-        </button>
-        <button className="btn btn-primary mx-1" onClick={handleCapitalize}>
-          Convert to Capitalize Each Word
         </button>
         <button
           type="submit"
@@ -75,14 +84,23 @@ export default function TextForm(props) {
           Speak
         </button>
       </div>
-      <div className="container my-3">
+      <div
+        className="container my-3"
+        style={{
+          color: props.mode === "dark" ? "white" : "black",
+        }}
+      >
         <h1>Your text summary</h1>
         <p>
           {text.split(" ").length - 1} Words and {text.length} Characters
         </p>
         <p>{0.008 * text.split(" ").length} Minutes to read text.</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter something in the text box to preview it here"}
+        </p>
       </div>
     </>
   );
